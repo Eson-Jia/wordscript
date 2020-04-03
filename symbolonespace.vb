785aa6eeae658b7c15b80744fdb0776964d45222
jiayuxin <13520723393@163.com>
2020-04-03 18:53:37 +0800

    修改符号标记

--- symbolonespace.vb
+++ symbolonespace.vb
@@ -1,7 +1,7 @@
 Sub OneWhiteSpaceAfterSymbol() '保证在标点后面的有且仅有一个空格
     Dim Sta As String, TheSymbol As String
-    Dim EnglishInterpunction() As Variant '(".", ",", ";", ":", "?", "!", "…", "-", "~", "(", ")", "<", ">")
-    EnglishInterpunction = Array("(\.)<([A-z]@)>", "(\,)<([A-z]@)>", "(\;)<([A-z]@)>", "(\:)<([A-z]@)>", "(\?)<([A-z]@)>", "(\!)<([A-z]@)>", "(\()<([A-z]@)>", "(\<)<([A-z]@)>", "(\>)<([A-z]@)>", "(…)<([A-z]@)>", "(-)<([A-z]@)>", "(~)<([A-z]@)>")
+    Dim EnglishInterpunction() As Variant '(".", ",", ";", ":", "?", "!",")", "<", ">")
+    EnglishInterpunction = Array("(\.)<([A-z]@)>", "(\,)<([A-z]@)>", "(\;)<([A-z]@)>", "(\:)<([A-z]@)>", "(\?)<([A-z]@)>", "(\!)<([A-z]@)>", "(\))<([A-z]@)>", "(\<)<([A-z]@)>", "(\>)<([A-z]@)>"
     '定义一个英文标点的数组对象
     Sta = "<([A-z]@)>"
     For N = 0 To UBound(EnglishInterpunction) '从数组的下标到上标间作一个循环
